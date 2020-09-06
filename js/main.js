dd7d86446a42c9953416dbc61ad1cbf139849ea3
Piotr Jakubowski <jakubowski656@gmail.com>
2020-09-06 18:48:52 +0200

    add rwd

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const overviews = document.querySelectorAll(
+    ".home-overviews__box__progress__loading-bar__percentage"
+  );
+
+  overviews.forEach((i) => {
+    i.style.width = `${i.dataset.percent}%`;
+    i.style.backgroundColor = `#4e74ff`;
+  });
+});
